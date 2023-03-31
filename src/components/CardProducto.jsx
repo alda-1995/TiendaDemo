87a0d9ec1a96c4cac1029685fa52bb4cc814ba27
alda-1995 <aldair@canalla.agency>
2023-03-31 12:28:51 -0600

    se agrega cards component

---
+++ src/components/CardProducto.jsx
@@ -0,0 +1,15 @@
+class CardProducto extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <div className="flex flex-colum">
+                <img src="" alt="" />
+            </div>
+        );
+    }
+}
+
+export default CardProducto;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,9 @@ class Home extends Component {
     render() {
         return (
             <div className="container">
-                
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+                    
+                </div>
             </div>
         );
     }
