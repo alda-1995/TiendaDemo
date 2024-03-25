2160088b78e47e4a59d1f5af11d268e70a1b4153
alda-1995 <aldair@canalla.agency>
2024-03-25 17:48:18 -0600

    base 3

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://alda-1995.github.io/TiendaDemo/"
+  base: "https://alda-1995.github.io/TiendaDemo"
 })
